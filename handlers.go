48c5f771423c7e2615e247811d7769f7bbe4f764
Kosench <ilyakosenko09@gmail.com>
2024-10-17 23:28:06 +1000

    add func LogoutHandler

--- handlers.go
+++ handlers.go
@@ -291,6 +291,48 @@ func LoginHandler(rw http.ResponseWriter, r *http.Request) {
 
 }
 
+func LogoutHandler(rw http.ResponseWriter, r *http.Request) {
+	log.Println("LogoutHandler Serving:", r.URL.Path, "from", r.Host)
+	d, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if len(d) == 0 {
+		rw.WriteHeader(http.StatusBadRequest)
+		log.Println("No input!")
+		return
+	}
+
+	var user = restdb.User{}
+	err = json.Unmarshal(d, &user)
+	if err != nil {
+		log.Println(err)
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if !restdb.IsUserValid(user) {
+		log.Println("User", user.Username, "exists!")
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	t := restdb.FindUserUsername(user.Username)
+	log.Println("Logging out: ", t.Username)
+	t.Active = 0
+	if restdb.UpdateUser(user) {
+		log.Println("User updated:", t)
+		rw.WriteHeader(http.StatusOK)
+	} else {
+		log.Println("User failed:", t)
+		rw.WriteHeader(http.StatusBadRequest)
+	}
+
+}
+
 func LoggedUsersHandler(rw http.ResponseWriter, r *http.Request) {
 	log.Println("LoggedUsersHandler Serving:", r.URL.Path, "from", r.Host)
 	var user = restdb.User{}
